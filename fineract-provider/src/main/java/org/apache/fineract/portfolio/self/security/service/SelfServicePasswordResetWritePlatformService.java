837c9bf1d4eea38228c6c4b14e8e1d4d548ebe1e
Pinchez25 <thuamwangi001@gmail.com>
2025-03-31 13:47:06 +0300

    Add SelfServicePasswordResetWritePlatformService interface for password reset functionality

---
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/self/security/service/SelfServicePasswordResetWritePlatformService.java
@@ -0,0 +1,11 @@
+package org.apache.fineract.portfolio.self.security.service;
+
+import org.apache.fineract.infrastructure.core.api.JsonCommand;
+import org.apache.fineract.infrastructure.core.data.CommandProcessingResult;
+
+public interface SelfServicePasswordResetWritePlatformService {
+
+    CommandProcessingResult requestPasswordReset(JsonCommand command);
+
+    CommandProcessingResult verifyAndUpdatePassword(JsonCommand command);
+} 
