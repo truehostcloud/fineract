1f17483dcbd8a1f1da1109089f10144db5151209
Pinchez25 <thuamwangi001@gmail.com>
2025-04-02 16:51:24 +0300

    Add DemographicStats class to encapsulate survey demographic data

---
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/survey/data/DemographicStats.java
@@ -0,0 +1,16 @@
+package org.apache.fineract.infrastructure.survey.data;
+
+import java.util.Map;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@Accessors(chain = true)
+public class DemographicStats {
+    private String demographicValue;
+    private int responseCount;
+    private double averageScore;
+    private Map<String, Double> questionAverages;
+} 
