a045786b70d5d38c6fc0a34d22b4ef07ada31005
Pinchez25 <thuamwangi001@gmail.com>
2025-04-02 16:50:59 +0300

    Add DemographicBreakdown class for enhanced survey analytics

---
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/survey/data/DemographicBreakdown.java
@@ -0,0 +1,21 @@
+package org.apache.fineract.infrastructure.survey.data;
+
+import java.util.Map;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@Accessors(chain = true)
+public class DemographicBreakdown {
+    private String demographicField;
+    private Map<String, DemographicStats> statsByValue;
+    
+    // Enhanced analytics fields
+    private Map<String, Double> correlationWithScores;
+    private Map<String, Map<String, Integer>> demographicResponsePatterns;
+    private Map<String, Double> demographicAverageScores;
+    private Map<String, Map<String, Double>> questionAveragesByValue;
+    private Map<String, Double> standardDeviationsByValue;
+} 
