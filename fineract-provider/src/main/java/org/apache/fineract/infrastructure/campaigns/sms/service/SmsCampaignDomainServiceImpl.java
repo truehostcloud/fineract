f399351fdebdc62271a3040609213a52fe853181
Pinchez25 <thuamwangi001@gmail.com>
2025-07-16 09:27:53 +0300

    Rename SMS campaign parameter 'Currency' to 'LoanCurrencyCode' for clarity and consistency

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/campaigns/sms/service/SmsCampaignDomainServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/campaigns/sms/service/SmsCampaignDomainServiceImpl.java
@@ -320,7 +320,7 @@ public class SmsCampaignDomainServiceImpl implements SmsCampaignDomainService {
         smsParams.put("mobileNo", client.mobileNo());
         smsParams.put("LoanAmount", loan.getPrincipal());
         smsParams.put("LoanOutstanding", loanTransaction.getOutstandingLoanBalance());
-        smsParams.put("Currency", loan.getCurrency().getCode());
+        smsParams.put("LoanCurrencyCode", loan.getCurrency().getCode());
         smsParams.put("PayoffAmount", loan.getSummary().getTotalOutstandingAmount());
         smsParams.put("loanId", loan.getId());
         smsParams.put("LoanAccountId", loan.getAccountNumber());
